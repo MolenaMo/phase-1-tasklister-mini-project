649d622cc4a154b5b3206d21af2ae0b18c5da39e
Monique Lena Moore <moniquelmoore06@gmail.com>
2022-04-28 23:54:41 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+// //or this below
+// document.querySelector('form').addEventListener('click', alertCallback)
+// //by creating this function below--
+// function alertCallback(){
+//   return alert("HI")
+// }
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.querySelector('#new-task-description').value)
+    form.reset()
+    // alert("e.target")
+  })})
+  function buildToDo(todo){
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = "x"
+    p.textContent = `${todo} `
+    p.appendChild(btn)
+    document.querySelector('#list').appendChild(p)
+  }
+    function handleDelete(e){
+      e.target.parentNode.remove()
+    }
